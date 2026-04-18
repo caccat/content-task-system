da809f92d236485fe7cc05c662d7cee97118623f
caccat <162131592+caccat@users.noreply.github.com>
2026-04-19 04:02:36 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.');
-          const ext = info[info.length - 1];
-          return `assets/[name]-[hash][extname]`;
-        },
-      },
-    },
-  },
 })
